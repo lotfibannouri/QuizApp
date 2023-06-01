5523fbd569b979060e80e72bd0caab1ccabcab94
Mohamed-Mansouri <mansouri.med.contact@gmail.com>
2023-06-01 14:40:49 +0100

    add Ignore json nulls

---
+++ Authentication.web/utility/Extentions.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http;
+using System.Runtime.Serialization;
+using System.Text.Json;
+using System.Text;
+
+namespace Authentication.web.utility
+{
+    public static class Extentions
+    {
+        public static async Task<HttpResponseMessage> PostAsJsonAsync(this HttpClient client,string Api,object obj,bool IgnoreNulls)
+        {
+            var jsonSerializerOptions = new JsonSerializerOptions
+            {
+                IgnoreNullValues = IgnoreNulls
+            };
+            var json = JsonSerializer.Serialize(obj, jsonSerializerOptions);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            HttpResponseMessage httpResponseMessage = await client.PostAsync("/api/Question/AddQuestion",content);
+            return httpResponseMessage;
+        }
+    }
+}
